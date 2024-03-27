0a46980e651ef120b20e8ef386db60fb92d8ce87
ranhai613 <85445781+ranhai613@users.noreply.github.com>
2024-03-27 19:20:21 +0900

    add eventlistener to copy and paset button

--- weblatereplace.user.js
+++ weblatereplace.user.js
@@ -3,20 +3,32 @@
 // @author          ranhai613
 // @description     This is a script that replaces certain parts of deepl translation in Weblate.
 // @include         /^https?://weblate.hyperq.be/translate/.*$/
-// @version         1.0
+// @version         1.1
 // ==/UserScript==
 
 (() => {
+	var used = false;
     var textarea = document.getElementsByClassName('translation-editor')[0];
-    textarea.addEventListener('input', Replace);
-    var used = false;
-    function Replace(){
-        if(used){
-            return;
-        }
-        document.getElementsByClassName('translation-editor')[0].value = Convert(textarea.value);
-        used = true;
-    }
+  	var copybtn;
+  	var parent = document.getElementById('machinery-translations');
+  	var observer = new MutationObserver((mutations) => {
+    		mutations.forEach((mutation) => {
+        		var copybtns = document.getElementsByClassName('js-copy-machinery');
+          		copybtn = copybtns[copybtns.length - 1];
+      			copybtn.addEventListener('click', () => {
+					textarea.addEventListener('input', Replace);
+        		});
+    		});
+		});
+  	var config = {
+   			childList: true
+		};
+  	observer.observe(parent, config);
+	function Replace(){
+		if(used) return;
+		textarea.value = Convert(textarea.value);
+		used = true;
+	}
     function Convert(text){
         var textarry = text.split(/["+「+」+]/);
         for(var i = 0; i < textarry.length; i++){
@@ -29,6 +41,8 @@
         newtext = newtext.replace(/\.\.\./g, '……');
         newtext = newtext.replace(/!/g, '！');
         newtext = newtext.replace(/\?/g, '？');
+      	newtext = newtext.replace(/よそ者/g, 'よぉそ者');
+      	newtext = newtext.replace(/多元宇宙/g, 'マルチバース');
         return newtext;
     }
 })();
