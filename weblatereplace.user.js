338e7b161054f118097d868c46d8a1d35388df1b
ranhai613 <85445781+ranhai613@users.noreply.github.com>
2024-03-29 19:38:23 +0900

    Added some words

--- weblatereplace.user.js
+++ weblatereplace.user.js
@@ -3,32 +3,53 @@
 // @author          ranhai613
 // @description     This is a script that replaces certain parts of deepl translation in Weblate.
 // @include         /^https?://weblate.hyperq.be/translate/.*$/
-// @version         1.0
+// @version         1.1
 // ==/UserScript==
 
 (() => {
-    var textarea = document.getElementsByClassName('translation-editor')[0];
-    textarea.addEventListener('input', Replace);
-    var used = false;
-    function Replace(){
-        if(used){
-            return;
-        }
-        document.getElementsByClassName('translation-editor')[0].value = Convert(textarea.value);
-        used = true;
+    var pattdict = {
+        '......': '……',
+        '...': '……',
+        '!': '！',
+        '?': '？',
+        'よそ者': 'よぉそ者',
+        '多元宇宙': 'マルチバース',
+        'ナメクジ': 'スラッグ',
     }
-    function Convert(text){
-        var textarry = text.split(/["+「+」+]/);
-        for(var i = 0; i < textarry.length; i++){
-            if(i % 2 == 1){
-                textarry[i] = `「${textarry[i]}」`;
-            }
+
+
+    var onclick = false
+    var textarea = document.getElementsByClassName('translation-editor')[0];
+    textarea.addEventListener('input', () => {
+        if (!onclick) return;
+        textarea.value = Convert(textarea.value);
+        onclick = false;
+    });
+    var parent = document.getElementById('machinery-translations');
+    var observer = new MutationObserver((mutations) => {
+        mutations.forEach(() => {
+            var copybtns = document.getElementsByClassName('js-copy-machinery');
+            var copybtn = copybtns[copybtns.length - 1];
+            copybtn.addEventListener('click', () => {
+                onclick = true;
+            });
+        });
+    });
+    var config = {
+        childList: true
+    };
+    observer.observe(parent, config);
+
+
+    function Convert(text) {
+        var textarry = text.split(/["「」]+/);
+        for (var i = 0; i < textarry.length; i++) {
+            if (i % 2 == 1) textarry[i] = `「${textarry[i]}」`;
         }
         var newtext = textarry.join('');
-        newtext = newtext.replace(/\.\.\.\.\.\./g, '……');
-        newtext = newtext.replace(/\.\.\./g, '……');
-        newtext = newtext.replace(/!/g, '！');
-        newtext = newtext.replace(/\?/g, '？');
+        for (var key in pattdict) {
+            newtext = newtext.replaceAll(key, pattdict[key]);
+        }
         return newtext;
     }
 })();
